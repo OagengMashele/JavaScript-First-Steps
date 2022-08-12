ca6e910cbc199526f5eb62b07fdfc87947f8d9ec
omashele021 <omashele021@student.wethinkcode.co.za>
2022-08-12 11:59:21 +0200

    Did the 1st,2nd and 3rd steps for Image gallery

--- JavaScript-building-blocks/Image-gallery/script.js
+++ JavaScript-building-blocks/Image-gallery/script.js
@@ -6,13 +6,29 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const imageFilenames = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
+
 /* Declaring the alternative text for each image file */
+const altText =  {
+    "pic1.jpg" : "eye",
+    "pic2.jpg" : "rock",
+    "pic3.jpg" : "pansies",
+    "pic4.jpg" : "tomb",
+    "pic1.jpg" : "moth and leaf"
+}
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of imageFilenames){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'imageFilenames/${image}');
+    newImage.setAttribute('alt', altText[image]);
+    thumbBar.appendChild(newImage);
+
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
